4d77d7fb43b5f5f0c21c447cb6a06496520419bd
Paitoon Phuchaiyang <paitoon.joke9@gmail.com>
2017-03-26 16:27:14 +0700

    fix

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var cors = require('cors')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.set('port', (process.env.PORT || 4000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
 	res.send('Hello')
-})
+});
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
-})
+});
